15a71eef09fbd12fff32f143d9fe9f977c4c5b63
Eshi12 <eshi12k@gmail.com>
2024-12-15 12:00:51 +0530

    Create projectController.js

---
+++ projectController.js
@@ -0,0 +1,20 @@
+const Project = require('../models/project');
+
+exports.getAllProjects = async (req, res) => {
+  try {
+    const projects = await Project.find();
+    res.json(projects);
+  } catch (err) {
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
+
+exports.addProject = async (req, res) => {
+  try {
+    const newProject = new Project(req.body);
+    await newProject.save();
+    res.status(201).json(newProject);
+  } catch (err) {
+    res.status(400).json({ message: 'Error creating project' });
+  }
+};
