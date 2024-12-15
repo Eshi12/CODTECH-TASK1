7ae0af5403480b0daad02ce742e79ef106b25fb0
Eshi12 <eshi12k@gmail.com>
2024-12-15 12:01:33 +0530

    Create blogController.js

---
+++ blogController.js
@@ -0,0 +1,20 @@
+const Blog = require('../models/blog');
+
+exports.getAllBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find();
+    res.json(blogs);
+  } catch (err) {
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
+
+exports.addBlog = async (req, res) => {
+  try {
+    const newBlog = new Blog(req.body);
+    await newBlog.save();
+    res.status(201).json(newBlog);
+  } catch (err) {
+    res.status(400).json({ message: 'Error creating blog' });
+  }
+};
